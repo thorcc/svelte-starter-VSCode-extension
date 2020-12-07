cc1c1666ac389e2b3e74fbbf26d89beb1791d1e0
thorcc <thorchrcow@gmail.com>
2020-12-07 15:03:29 +0100

    added createVGSwebpack

--- extension.js
+++ extension.js
@@ -54,7 +54,7 @@ function activate(context) {
 		terminal.sendText("npm run dev");
 	});
 
-	let createVGSwebpack = vscode.commands.registerCommand('svelte-starter.createVGS', function (uri) {
+	let createVGSwebpack = vscode.commands.registerCommand('svelte-starter.createVGSwebpack', function (uri) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
